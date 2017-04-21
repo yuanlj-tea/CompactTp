434bb2d16cd6e1a6aa8cf75739942ce0170cf608
Allen <1107806823@qq.com>
2017-04-21 08:31:14 +0800

    config配置

--- index.php
+++ index.php
@@ -4,5 +4,10 @@ $whoops = new \Whoops\Run;
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
 
+define("MOMOMA_PATH", dirname(__FILE__)); //项目根目录
 
-echo $a;
+// Config
+$conf = new \Noodlehaus\Config(MOMOMA_PATH. '/Config/conf.php');
+echo $conf->get('debug');
+//echo $conf['debug'];
+
